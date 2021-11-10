c4296c0632297fd15a8849223157a6c2e4415de7
UnprecedentedWolf <UnprecedentedWolf@gmail.com>
2021-11-10 02:17:34 +0100

    Changed the Nimitz command so that it can be triggered on any user of the discord (for example "!!Nimitz bio2#4246"). Requires two new images: https://cdn.discordapp.com/attachments/745357907692093505/907794897627660288/jessie.png https://cdn.discordapp.com/attachments/745357907692093505/907795010911625216/circle.png

--- cogs/nimitz.py
+++ cogs/nimitz.py
@@ -1,13 +1,15 @@
 # NOTE: This requires ImageMagick installed on the device https://imagemagick.org/script/download.php
-# It also needs this image template saved as 'nimitz.png' https://cdn.discordapp.com/attachments/902633836108709978/902635626665504799/nimitz.png
+# It also needs these two images:
+# https://cdn.discordapp.com/attachments/745357907692093505/907794897627660288/jessie.png
+# https://cdn.discordapp.com/attachments/745357907692093505/907795010911625216/circle.png
 
 from config import config, db
 
-import Wand
 from wand.drawing import Drawing
 from wand.image import Image
 from wand.color import Color
 from wand.font import Font
+from urllib.request import Request,urlopen
 
 import discord
 from discord.ext import commands
@@ -20,27 +22,45 @@ class NimitzCog(commands.Cog):
         
     @commands.command(name='nimitz')
     @commands.max_concurrency(1, per=commands.BucketType.default, wait=True)
-    async def _make_nimitz_meme(ctx):
+    async def _make_nimitz_meme(ctx,arg):
+        if arg == 'Nimitz':
+            Jessie = 'Nimitz#9070'
+        else:
+            Jessie = arg
         NimitzLines = ['','','']
         NimitzCounter = 2
         async for message in ctx.history(limit=200,before=ctx.message):
-            if message.author.id == 208427860560445441 and counter >= 0:
+            if str(message.author) == Jessie and counter >= 0:
                 NimitzLines[NimitzCounter] = message.content
+                JessieUser = message.author
+                JessieName = JessieUser.display_name
                 NimitzCounter -= 1
         if '' in NimitzLines:
-            ctx.send("Failed to make fun of nimitz.")
+            ctx.send("Failed to fetch last 3 messages of",Jessie)
         else:
-            with Image(filename='nimitz.png') as img:
-                with Drawing() as draw:
-                    NimitzFont = wand.font.Font('impact', size=0, color='White', antialias=True)
-                    img.caption(NimitzLines[0], left=16, top=111, width=517, height=96, font=memetext, gravity='south_west')
-                    img.caption(NimitzLines[1], left=16, top=551, width=517, height=96, font=memetext, gravity='south_west')
-                    img.caption(NimitzLines[2], left=16, top=763, width=517, height=96, font=memetext, gravity='south_west')
-                    img.save(filename='output.png')
-                await ctx.send(file=discord.File('output.png'))
-           
-            
+            JessieRequest = Request(JessieUser.avatar_url,headers={'User-Agent': 'Mozilla/5.0'})
+            JessieAv = urlopen(JessieRequest).read()
+            with Image(filename='jessie.png') as img:
+                with Image(blob=jessie_av) as av:
+                    av.resize(90,90)
+                    av.save(filename='output.png')
+                    av.alpha_channel = True
+                    with Drawing() as draw:
+                        draw.alpha(90,90,'floodfill')
+                    with Image(filename='circle.png') as circle:
+                        av.composite_channel(channel='alpha',image=circle,operator='replace')
+                        av.save(filename='output.png')
+                    with Drawing() as draw:
+                        memetext = Font('impact', size=0, color='White', antialias=True)
+                        img.caption(NimitzLines[0], left=16, top=111, width=517, height=96, font=memetext, gravity='south_west')
+                        img.caption(NimitzLines[1], left=16, top=551, width=517, height=96, font=memetext, gravity='south_west')
+                        img.caption(NimitzLines[2], left=16, top=763, width=517, height=96, font=memetext, gravity='south_west')
+                        img.caption(JessieName+' what the fuck are you talking about?', left=254, top=998, width=293, height=67, font=memetext, gravity='south_west')
+                        img.composite(av, left=82, top=3)
+                        img.composite(av, left=122, top=460)
+                        img.composite(av, left=113, top=664)
+                        img.save(filename='output.png')
+                    await ctx.send(file=discord.File('output.png'))
             
 def setup(bot):
     bot.add_cog(NimitzCog(bot))
-    
